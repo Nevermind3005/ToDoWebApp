086585291876c7dab01006cfd934a4c9b853ae50
jakubelis <jakub.elis007@gmail.com>
2023-01-31 20:46:39 +0100

    Edit useFetch

--- web/src/api.ts
+++ web/src/api.ts
@@ -2,6 +2,7 @@ const baseUrl = 'http://localhost:5048';
 const endpoints = {
     register: '/api/Auth/register',
     login: '/api/Auth/login',
+    token: '/api/Auth/token',
     user: {
         me: '/api/User/me',
     },
--- web/src/useFetch.ts
+++ web/src/useFetch.ts
@@ -1,24 +1,52 @@
-import { getAccessToken } from './AccessToken';
+import { getAccessToken, setAccessToken } from './AccessToken';
+import { baseUrl, endpoints } from './api';
 
 const useFetch = () => {
     const auth = () => {
         return 'Bearer ' + getAccessToken();
     };
 
-    const request = (method: string) => {
-        return (url: string, body?: string) => {
-            let headers: any = {
+    const createRequestOptions = (method: string, body: string) => {
+        return {
+            method: method,
+            headers: {
                 Authorization: auth(),
-            };
-            const reqOptions: RequestInit = {
-                method: method,
-                headers,
-            };
-            if (body) {
-                headers['Content-Type'] = 'application/json';
-                reqOptions.body = body;
-            }
-            return fetch(url, reqOptions);
+                'content-type': 'application/json',
+                body: body,
+            },
+        };
+    };
+
+    const fetchData = (
+        url: string,
+        method: string,
+        body: string,
+        retry: boolean = false
+    ): Response | null => {
+        fetch(url, createRequestOptions(method, body))
+            .then(async (response) => {
+                if (response.status === 401 && retry) {
+                    await refreshToken();
+                    return fetchData(url, method, body);
+                }
+                return response;
+            })
+            .catch((error) => console.error(error.message));
+        return null;
+    };
+
+    const refreshToken = async () => {
+        await fetch(baseUrl + endpoints.token, { method: 'post' })
+            .then((response) => response.text())
+            .then((data) => {
+                setAccessToken(data);
+            });
+    };
+
+    const request = (method: string) => {
+        return (url: string, body: string = '') => {
+            let response = fetchData(url, method, body, true);
+            return response;
         };
     };
 
